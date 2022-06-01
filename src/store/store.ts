5bc67013d15c1a914cb199fd5ef30eb177d34f62
matt <you@example.com>
2022-06-01 15:17:35 -0500

    clean up

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import characterReducer from './reducers/characterReducer';
 
-// dummy reducer will be replaced when the real reducers are created
+
 const store = configureStore({
   reducer: {
     characters: characterReducer,
